03bf4da60340e9dd68470f0a2db2cebe82898fa5
wulibao <2398295101@qq.com>
2017-02-21 19:35:43 +0800

    recyclerview

---
+++ app/src/main/java/com/example/administrator/android220/Dog.java
@@ -0,0 +1,13 @@
+package com.example.administrator.android220;
+
+/**
+ * Created by Administrator on 2017/2/21.
+ */
+public class Dog {
+    String name;
+    int  imgId;
+    public Dog(String name,int  imgId){
+        this.name=name;
+        this.imgId=imgId;
+    }
+}
